5d4d9a950f7597cdc426df4bb691fc7ac41c3259
happytaohaha <1178984504@qq.com>
2019-03-16 19:55:30 +0800

    test

---
+++ cmake-build-debug/CMakeFiles/hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world.dir/main.cpp.obj"
+  "hello_world.pdb"
+  "hello_world.exe"
+  "hello_world.exe.manifest"
+  "libhello_world.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
