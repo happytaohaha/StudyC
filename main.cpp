f037c0d6e1c515e5778b5eb6199ff570d5ed9d60
happytaohaha <1178984504@qq.com>
2019-03-18 17:41:42 +0800

    各种类型的长度

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
-#include <stdio.h>//include  头文件  <> 一般是标准模板中的  去系统中去寻找
-#include <math.h>
-//宏定义常量
-#define PRICE 30
+#include <stdio.h>  //标准输入输出
+//各种类型的长度
 int main() {
 
-    int num , total;
-    num = 10;
-    total = num * PRICE;
-    //%d  是整数
-    printf("total=%d\n",total);
-
+    printf("float :%d\n", sizeof(float));
+    printf("int :%d\n", sizeof(int));
+    printf("short int :%d\n", sizeof(short int));
+    printf("unsigned int :%d\n", sizeof(unsigned int));
+    printf("unsigned short int :%d\n", sizeof(unsigned short int));
+    printf("long int :%d\n", sizeof(long int));
     return 0;
 }
-//undef  需要带上定义的是什么东西  符号常量
-#undef PRICE
