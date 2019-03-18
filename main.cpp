ac2e1ba2d86a67a53e72b6573109ba0a4c7157c3
happytaohaha <1178984504@qq.com>
2019-03-18 19:11:15 +0800

    各种类型的长度

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <stdio.h>  //标准输入输出
-//各种类型的长度
+//整形的数据溢出
 int main() {
 
-    printf("float :%d\n", sizeof(float));
-    printf("int :%d\n", sizeof(int));
-    printf("short int :%d\n", sizeof(short int));
-    printf("unsigned int :%d\n", sizeof(unsigned int));
-    printf("unsigned short int :%d\n", sizeof(unsigned short int));
-    printf("long int :%d\n", sizeof(long int));
+    int a, b;
+    a = 32767;
+    b = a + 1;
+    //这个可以是因为a b 是4个字节  32位  范围大。
+    printf("a=%d,b=%d\n",a,b);
+
+    short int c, d;
+    c = 32767;
+    d = c + 1;
+    //这个可以是因为c d 是2个字节  16位  范围小， 所以错误。
+    printf("c=%d,d=%d\n",c,d);
+/**
+ * 输出结果
+ * a=32767,b=32768
+   c=32767,d=-32768
+ */
     return 0;
 }
