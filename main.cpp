df3ff01a5759745f25173a2800a65a385983e4ca
happytaohaha <1178984504@qq.com>
2019-03-18 17:26:05 +0800

    预处理-宏定义

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
-#include <stdio.h>//include  头文件  <> 一般是标准模板中的
+#include <stdio.h>//include  头文件  <> 一般是标准模板中的  去系统中去寻找
 #include <math.h>
-//sin计算
+//宏定义常量
+#define PRICE 30
 int main() {
-    //定义pi   cospi  = -1    acos （-1）  = pi
-    const double pi = acos(-1.0);
-    double x, r, s;
-    //\n换行   printf  输出一段话
-    printf("input number:\n");
-    //%lf  左边的一个浮点数
-    scanf("%lf",&x);
-    //角度转弧度   也就是是多少pi
-    r = x * pi / 180;
-    //这里的都是拿弧度进行计算的
-    s = sin(r);
-    printf("sin of %lf is %lf\n",x,s);
 
-    return 0;  //return  0  是正常返回的，如果不是0  则是错误的
+    int num , total;
+    num = 10;
+    total = num * PRICE;
+    //%d  是整数
+    printf("total=%d\n",total);
+
+    return 0;
 }
+//undef  需要带上定义的是什么东西  符号常量
+#undef PRICE
