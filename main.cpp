47a89119db14591341e059d5f491f4623f8295e9
happytaohaha <1178984504@qq.com>
2019-03-19 19:23:46 +0800

    类型转换再探究

--- main.cpp
+++ main.cpp
@@ -1,29 +1,21 @@
 #include <stdio.h>  //标准输入输出
-//字符char 的应用
+//类型转换再探究
 int main() {
-    /**
-     *  字符常量 ''   字符串常量  " "
-     */
-    char a, b, c, d, e, f;
-    // ascill码所对应的 值  120 对应 x   121 =  y
-    a = 120;
-    b = 121;
-    //单引号括字符
-    c = 'x';
-    d = 'y';
-    printf("a=%d,a=%c\n",a,a);
-    printf("b=%d,b=%c\n",b,b);
-    printf("c=%d,c=%c\n",c,c);
-    printf("d=%d,d=%c\n",d,d);
-    //小写变大写    小写 -32
-    e = a - 32;
-    f = b - 32;
-    printf("e=%d,e=%c\n",e,e);
-    printf("f=%d,f=%c\n",f,f);
+
+    float PI = 3.14159;
+    int s ;
+    double s1 ;
+    int r = 1;
+    //计算时先变成double  然后  运算完再转换为  int
+    s= r * r * PI;
+    s1= r * r * PI;
+    printf("%d\n",s);
+    printf("%f\n",s1);
 
 
-
-
+    float f = 5.75;
+    //强制变为整形  是临时的
+    printf("(int)f =%d,f =%f\n",(int)f,f);
 
 
 
