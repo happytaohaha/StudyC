87a9280f19876b7ac63ec574d223e056edc3de0c
happytaohaha <1178984504@qq.com>
2019-03-21 21:19:40 +0800

    a[]  === *a

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
 #include <stdio.h>  //标准输入输出
 #include <math.h>
-//c中指针++问题
+//a[]  === *a
+// *a 取值   a 为
+/**
+ * a[]  === *a
+ * 探究
+ * 1.   a[1] = 1 ;这'=' 是赋值号
+ * 2.   a[1] =a[2] ;这个是--> 的意思   a[1]和a[2]指向同一个数
+ * * a 同理
+ * 因此解决 a[]  === *a 的原因    a 是地址  * ,[]  是 取值
+ */
+void converse(int a[],int len){
+    int mid, i, j;
+    mid = len / 2;
+    for( i = 0; i < mid ; i++) {
+        j = a[i];//将其中的值给j
+        a[i] = a[len - 1 - i];// 将值赋给指针
+        a[len - 1 - i] = j;//交换地址
+    }
+}
 int main() {
-    int *p, i, a[10];
-    p = a;
-    for(i = 0; i < 10; i++){
-
-        //scanf("%d", p++);  //这样错误的原因是 一直p++ p的地址被增加了  p增加后变成了下一个  跟++有关
-        scanf("%d", p+i);
-//        p--;
-//        printf("%d",*p);
-
+    int a[11]={ 3, 7, 9, 11, 0, 6, 7, 5, 4, 2,11};
+    //a就是地址
+    converse(a,11);
+    for(int i = 0; i < 11; i++){
+        printf("%d ",a[i]);
     }
-    printf("\n");
-    //p = p-10;
-    for(i = 0; i < 10; i++, p++){
-        printf("%d",*p);
-    }
-    /**   总结
-     *     1. 解决方案1  用 p+i  不变指针
-     *      2. 把p 调回去
-     *      推荐第一种
-     */
-
     return 0;
 };
+
