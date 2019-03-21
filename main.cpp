ab260575b1493897e70dbbef27ef3fc4faeb7a34
happytaohaha <1178984504@qq.com>
2019-03-21 17:06:51 +0800

    函数声明和实现

--- main.cpp
+++ main.cpp
@@ -1,32 +1,27 @@
 #include <stdio.h>  //标准输入输出
 #include <math.h>
-//冒泡排序
+
+//先需要定义函数 定义必须放到最前面
+int f(int a, int b);
+
 int main() {
-    int a[10];
 
-    //输入模块
-    printf("请输入10个数字:\n");
-    for(int i = 0; i<10; i++){
-        //这个如果打空格一直输入也可以   它会自动把输入的放进去， 超出的扔掉
-        scanf("%d", &a[i]);
-    }
 
-    //冒泡排序模块
-    //外层只需要 9个数 内层  最大9个数
-    int temp;
-    for(int i = 0; i<9; i++){
-        for(int j = i+1;j<10;j++){
-            if(a[i]>a[j]){
-                temp = a[j];
-                a[j] = a[i];
-                a[i] = temp;
-            }
-        }
-    }
-
-    //输出模块
-    for(int i = 0; i<10; i++){
-        printf("%d ", a[i]);
-    }
+    int i = 2, p;
+    //这里 自右向左调用
+    p = f(i, ++i);
+    printf("%d\n", p);
     return 0;
+}
+
+int f(int a, int b){
+    int c;
+    if( a > b){
+        c = 1;
+    }else if( a == b){
+        c = 0;
+    }else{
+        c = -1;
+    }
+    return c;
 }
