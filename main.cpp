bd13a01fac4ddf2bdf1bca95026f394199545263
happytaohaha <1178984504@qq.com>
2019-03-21 17:20:24 +0800

    指针入门

--- main.cpp
+++ main.cpp
@@ -1,27 +1,20 @@
 #include <stdio.h>  //标准输入输出
 #include <math.h>
 
-//先需要定义函数 定义必须放到最前面
-int f(int a, int b);
 
+//指针入门
 int main() {
-
-
-    int i = 2, p;
-    //这里 自右向左调用
-    p = f(i, ++i);
-    printf("%d\n", p);
+    /**
+     * 指针声明是 *
+     * 而 在运算中 * 取值   &取地址
+     */
+    //pointer 是一个指针  * 只是声明的时候说明他是指针
+    int * pointer;
+    int a =2000;
+    //pointer指向了a所在的地址  int 和上面的指针int 值需要相同
+    pointer = &a;
+    //这里的* 是取值的意思 因此*pointer 取得是 pointer 指向的地址中的 a的值
+    printf("a=%d",*pointer);
     return 0;
 }
 
-int f(int a, int b){
-    int c;
-    if( a > b){
-        c = 1;
-    }else if( a == b){
-        c = 0;
-    }else{
-        c = -1;
-    }
-    return c;
-}
