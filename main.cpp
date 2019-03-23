5a78f80475e4edf5597423334f99fd5292b58e8f
happytaohaha <1178984504@qq.com>
2019-03-23 08:53:26 +0800

    返回指针的函数

--- main.cpp
+++ main.cpp
@@ -1,45 +1,51 @@
 #include <stdio.h>  //标准输入输出
 #include <math.h>
-//函数指针  指向函数的指针  多态
-/**
-  * 类似于多态的东西
-  */
-int max(int x,int y){
-    int z;
-    if( x > y){
-        z = x;
-    }else{
-        z = y;
-    }
-    return z;
-};
-int min(int x,int y){
-    int z;
-    if( x < y){
-        z = x;
-    }else{
-        z = y;
-    }
-    return z;
-};
-/**
- * int (*p)()   函数指针  指向函数的指针
- */
-int process(int x ,int y, int (*p)(int a,int b)){
-    return (*p)(x,y);
-};
+//返回指针的函数
+double* search( double(*pointer)[4],int n);
 int main() {
-    int a = 1,b = 2;
-    int ma , mi;
-    ma =process( a,b,max);
-    mi =process( a,b,min);
-    printf("max =%d",ma);
-    printf("max =%d",mi);
+    /**
+     * 总结 : 1.在复制循环体的时候  要检查变量 ，以防错误， for()  内的所有参数检查
+     *      2. 在复制的时候一定注意！！！！！！！！
+     */
+    double score[][4]={{60.0,70.0,80.5,90.5},{56.0,89.0,67.0,88.0},{34.2,78.5,90.0,60.0}};
+    double *p;
+    int i, m, j, k, flag;
+    printf("Please enter the number of Student:");
+    scanf("%d",&m);
+    p =search(score, m);
+    for( i = 0; i < 4; i++){
+        printf("%lf\t",*(p+i));
+    }
+    printf("\n");
+
+    //找出不及格的学生
+    printf("Not Passed Students:");
+    for( i = 0; i < 3; i++){
+        flag = 0;
+        p =search(score, i);
+        for( j = 0; j < 4; j++){
+            if(*(p+j)<60){
+                flag = 1;
+            }
+        }
+        if(flag ==1){
+            printf("\n学生%d\t",i);
+            for( k = 0; k < 4; k++){
+                printf("%lf\t",*(p+k));
+            }
+        }
+    }
+
     return 0;
 };
-/**  总结
- * 记忆方法放在里面就是  （*p） 就是指针
- * 函数指针  指向函数的指针  int (* p) ();
- * 数组指针    指向数组的指针 int  (*p)[4];
- * 指针数组  存放指针的数组   int * p[4];
+/**
+ * 传入地址 返回地址  对于  * 来说
  */
+double* search( double(* pointer)[4],int n){
+    double* p;
+    //*(pointer + n) 就是一维  pointer[n]  取地址
+    p = *(pointer + n);
+    //ruturn  的是地址
+    return p;
+};
+
