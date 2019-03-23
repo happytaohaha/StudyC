648ce3e903620cb62679247df9ec21bc655262e4
happytaohaha <1178984504@qq.com>
2019-03-23 18:09:18 +0800

    结构体定义的三种方式

--- main.cpp
+++ main.cpp
@@ -1,30 +1,45 @@
 #include <stdio.h>
-//结构体占内存大小计算
+//结构体定义的三种方式
+/**
+ * 1. 先声明结构体再定义变量
+ */
+#if (0)
+ struct student{
+     int num;
+     char sex;
+     int age;
+     float score;
+     char name[20];
+     char addr[30];
+ };
+ student student1,student2;
+#endif
+/**
+* 2. 在声明结构体的同时一起定义变量
+*/
+#if (0)
 struct student{
+     int num;
+     char sex;
+     int age;
+     float score;
+     char name[20];
+     char addr[30];
+ }student1,student2;
+#endif
+/**
+* 3. 直接定义结构体类型变量
+*/
+struct {
     int num;
     char sex;
     int age;
     float score;
     char name[20];
-    char addr[30];  //补齐的地方应该是这
-} student1;   // 4 +20+ 1 +4 +4 +30
-// 先把1  变成 4  4+ 20 + 4 +4 +4 +30  =66  不是4 的倍数 补齐
-//结构体中最大占用内存类型double类型大小(8)的整数倍，所以A最后的大小是32；
-struct tagPhone{
-    char A;
-    short C;
-    int B;
-};
+    char addr[30];
+}student1,student2;
+
 int main(int argc, char *argv[]){
-    printf("%d\n", sizeof(student));
-    printf("%d\n", sizeof(tagPhone));
     return 0;
 }
 
-/**
- * 结构体有对齐功能   她的存储方法都是按最长的倍数   最后计算出来结构体最终是最大类型的倍数  4  8 16 等
- * 结构体内存计算方法
- * https://blog.csdn.net/fb2058/article/details/15502071
- * 结构体有对齐功能
- * https://blog.csdn.net/lidoublewen/article/details/4468264
- */
