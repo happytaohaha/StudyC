4929cee4dbdf884e7b265a49aa46fe59aee9c0e8
happytaohaha <1178984504@qq.com>
2019-03-27 21:49:22 +0800

    大小写转换位运算

--- main.cpp
+++ main.cpp
@@ -2,28 +2,32 @@
 #include <cstring>
 #include <cstdlib>
 
-//文件的读写
+//位运算
+//大小写转换  A 65~90   a 65+32  97 ~122
 int main(int argc, char *argv[]){
-    FILE *fp;
-    //这个是在栈中直接分配的地址
-    char ch,fileName[20];
-    printf("Please input filename which you want write\n");
-    //fileName 已经是地址
-    scanf("%s",fileName);
+    char ch;
+
+    printf("请输入一个字母\n");
+    ch = getchar();
+    while (!(ch >= 'A' && ch <= 'z')|| (ch > 'Z' && ch < 'a')){
+        printf("输入错误请重新输入:\n");
+        ch = getchar();
+    }
+    if(ch & 32){ //  00010000  与的话  判断 第五位是否为1
+         ch = ch & 223;  //111   11011111
+    }else {   //A - a
+        ch = ch | 32;
+    }
+    printf("%c",ch);
+    /**
+     * a  = 3 ,b =4
+     * 交换
+     * a = a ^ b;  异或
+     * b = b ^ a;
+     * a = a ^ b;
+     */
 
 
-   if(!(fp =fopen("D:\\a.txt","a"))){
-       printf("can not open D:\\a.txt");
-       exit(0);
-   }
-   printf("Please input the senteces which you want write\n");
-   ch = getchar();//匹配 换行
-   ch = getchar();
-   while (ch != EOF){  //ctrl + Z
-       fputc(ch, fp);
-       ch = getchar();
-   }
-    fclose(fp);
     return 0;
 }
 
