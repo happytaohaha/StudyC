bbb2a24bbddaa89ae728209c306f5371db919f3d
happytaohaha <1178984504@qq.com>
2019-03-27 22:08:09 +0800

    循环移位

--- main.cpp
+++ main.cpp
@@ -2,28 +2,18 @@
 #include <cstring>
 #include <cstdlib>
 
-//移位运算符
+//循环移位
 int main(int argc, char *argv[]){
-    int a = 2;
-    int b = -2;
-    unsigned int c = 2;
-    unsigned  int d = 255;
-    a = a >> 1;   //箭头指哪别往那边移动
-    b = b >> 1;
-    c = c >> 1;
-    printf("%d\n",c);
-    printf("%d\n",a);
-    printf("%d\n",b);
-    a = a << 1;
-    b = b << 1;
-    c = c << 1;
-    printf("%d\n",c);
-    printf("%d\n",a);
-    printf("%d\n",b);
-
-    //有算数移位 带符号   正数  左移 右边补0  负数 左移  补1
-    // 逻辑移位不带符号   正数  左移 右边补0
-
+    unsigned char a, b, c;
+    int n;
+    printf("请输入需要循环右移的数字：");
+    scanf("%d", &a);
+    printf("请输入需要右移动的位数:");  //原始数  00000011
+    scanf("%d", &n);//1   00000001
+    b = a <<(sizeof(char) *8 - n ); //7   移动完低位补  10000000
+    c = a >>n;
+    c = c | b;          //10000001
+    printf("结果是: %c\n",c);
     return 0;
 }
 
