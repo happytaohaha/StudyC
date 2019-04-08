a3873339ae747e7ea62a83fc9dee21ffba9dc3cc
happytaohaha <1178984504@qq.com>
2019-04-08 16:08:06 +0800

    1007 素数对猜想

--- main.cpp
+++ main.cpp
@@ -1,33 +1,48 @@
 #include<stdio.h>
+#include <math.h>
 
+int isPrime(int i);
+//不能用bool
+//1007 素数对猜想
 /**
- *  非常简单的一个每一位识别
- *  用好 %10  /10
+ * 1.求解素数的方法  i < sqrt(a)  然后  比较 a %i == 0
+ * 2.等于号的使用是否正确
+ * 3. PAT 里面标准C 没有bool
+ * 4. 可以使用math.h
  */
-//1006 换个格式输出整数
 int main()
 {
-    int a = 0;
-    scanf("%d", &a);
-    int b = 0;
-    int s = 0;
-    int d = 0;
-    d = a % 10;
-    a = a / 10;
-    s = a % 10;
-    b = a / 10;
-    while(b > 0){
-        printf("%c",'B');
-        b--;
-    }
-    while(s > 0){
-        printf("%c",'S');
-        s--;
-    }
-    for(int i = 1; i <= d; i++){
-        printf("%d", i);
-    }
+    int a[10000] ={};
+    int num = 0;
+    scanf("%d",&num);
+    int count = 0;
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+    //素数的下标
+    int count1 = 2;
+    //差一个等于号因为可能最后一个数也为素数
+    for(int i = 3; i<= num; i++){
+        if(isPrime(i)){
+            count1++;
+            a[count1] = i;
+            if(a[count1] - a[count1-1]==2){
+                count ++;
+            }
 
+        }
+    }
+    printf("%d", count);
     return 0;
+}
+//必须是<=
+int isPrime(int i) {
+    int flag = 1;
+    for(int j = 2; j <= (int)sqrt(i); j ++){
+            if(0 == i % j){
+                flag = 0;
+            }
+    }
+    return flag;
 }
 
