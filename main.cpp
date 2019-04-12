3e284c3cc725d3d519a0f6b54094db5b7caf9827
happytaohaha <1178984504@qq.com>
2019-04-12 20:27:17 +0800

    1013 数素数

--- main.cpp
+++ main.cpp
@@ -1,87 +1,50 @@
 #include<stdio.h>
+#include <math.h>
 /**
- * 1.格式的问题 复制后要尽心检擦
- * 2.修改后也要先检查 不要直接跑
- * 3. 减少提交次数，这样会提高以后做题的正确率
- * 4. n1 -n2 +n3 -n4 ... 可能是 5n  也可能是 5n+1
+ * 难点 需要 计算前10000个素数，数据量小可以一个一个算
+ * 格式 考虑一开始有问题
  *
- * @return
  */
- //1012 数字分类
-int main() {
-    int a = 0;
-    int a1 = 0;
-    int a2 = -1;
-    int a3 = 0;
-    float a4 = 0;
-    int a5 = 0;
+//1013 数素数
+int isPrime(int x){
     int flag = 1;
-    int sum = 0;
-     int num = 0;
-     int n = 0;
-     scanf("%d", &n);
-     for(int i = 0;i < n; i++){
-         scanf("%d", &a);
-         switch( a%5){
-             case 0:
-                 if( a/5%2 == 0 ){
-                     a1 += a;
-                 }
-                 break;
-             case 1:
-                 if(a2 == -1){
-                     a2 = 0;
-                 }
-                 if(flag == 1){
-                     a2 += a;
-                     flag = 0;
-                 }else{
-                     a2 -= a;
-                     flag = 1;
-                 }
-                 break;
-             case 2:
-                 a3++;
-                 break;
-             case 3:
-                 sum += a;
-                 num++;
-                 break;
-             case 4:
-                 if(a5 < a){
-                     a5 = a;
-                 }
-                 break;
-             default:
-                 break;
-         }
-     }
-    if(num != 0)
-        a4 = sum/(float)num;
-    if(a1 == 0){
-        printf("N ");
-    }else{
-        printf("%d ", a1);
+    for(int i=2;i<=sqrt(x);i++)
+    {
+        if(x%i == 0)
+        {
+            flag=0;
+        }
     }
-     if(a2 == -1){
-         printf("N ");
-     }else{
-         printf("%d ", a2);
-     }
-     if(a3 == 0){
-         printf("N ");
-     }else{
-         printf("%d ", a3);
-     }
-     if(a4 == 0){
-         printf("N ");
-     }else{
-         printf("%.1f ", a4);
-     }
-     if(a5 == 0){
-         printf("N");
-     }else{
-         printf("%d", a5);
-     }
+    return flag;
+}
+int main() {
+    int a[10000];
+    //因为10000 比较小还可以求的出来
+    //1.先找出10000以内所有素数
+    a[0] = 2;
+    int j =1;
+    for(int i = 3; j < 10000; i++){//题为前10000个素数
+        if(isPrime(i)){
+            a[j] = i;
+            j++;
+        }
+    }
+    //2.按格式输出
+    int m, n;
+    int k = 1;
+    scanf("%d %d", &m, &n);
+    for(int i = m; i <= n; i++){
+        if(k %10 != 0 && i != n){
+           printf("%d ",a[i-1]);
+        }else if(k % 10 == 0){
+            printf("%d\n",a[i-1]);
+        }
+        else{
+            printf("%d",a[i-1]);
+        }
+        k++;
+    }
+
+
     return 0;
 }
