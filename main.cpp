522f43befd439fe117b5d4b11e2002d6241144e0
happytaohaha <1178984504@qq.com>
2019-04-14 09:49:36 +0800

    1018 锤子剪刀布

--- main.cpp
+++ main.cpp
@@ -1,50 +1,105 @@
 #include<stdio.h>
 /**
- *  1.注意字符与数字之间的转化    charA[i] -'0'   字符转 数字   (char)(a[0]+'0') 数字转字符
- *  2.考虑特殊情况 拿出来单独处理 1 2 前两位  和只有一位的情况
+ * 不知道为什么我这个编译不通过
  */
-// 1017 A除以B
-int main(){
-    char charA[1000] = "";
-    int a[1000] = {};
-    int b;
-    int c[1000] = {};
-    int d;
-    int tempYuShu;
-    int i = 0;
-    scanf("%s %d", charA, &b);
-    for(; charA[i] != '\0'; i++){
-        a[i] = charA[i] -'0';
-    }
-    int j = 0;
-    int k = 0;
-    int flag = 1;
-    if(a[0]/b){
-        c[0] = a[0] / b;
-        tempYuShu = a[0] % b;
-        j = 1;
-    }else if(a[1]=='\0'){
-        c[0] = a[0] / b;
-        tempYuShu = a[0] % b;
-        flag = 0;
+ //1018 锤子剪刀布
+ struct Person{
+     int B = 0;
+     int C = 0;
+     int J = 0;
+     int win = 0;
+     int lose = 0;
+     int  flat= 0;
+ } person1,person2;
+ int compleCJB(char a, char b){
+   if(a == 'J'){
+       switch (b){
+           case 'B':
+               return 1;
+           case 'C':
+               return -1;
+           default:
+               return 0;
+       }
+   }else if(a == 'B'){
+       switch (b){
+           case 'C':
+               return 1;
+           case 'J':
+               return -1;
+           default:
+               return 0;
+       }
+   }else{
+       switch (b){
+           case 'J':
+               return 1;
+           case 'B':
+               return -1;
+           default:
+               return 0;
+       }
+   }
+ };
+void addResult(Person &personA,Person &personB, int result, char a, char b){
+    if(result==1){
+        personA.win ++;
+        personB.lose ++;
+        if(a =='J'){
+            personA.J++;
+        }else if(a == 'C'){
+            personA.C++;
+        }else{
+            personA.B++;
+        }
+    }else if(result ==0){
+        personA.flat ++;
+        personB.flat ++;
     }else{
-        c[0] =(a[0]*10 +a[1])/b;
-        tempYuShu = (a[0]*10 +a[1]) % b;
-        j = 2;
-    }
-    k++;
-    if(flag==1){
-        for(; j < i; j++){
-            c[k] = (tempYuShu *10 +a[j])/b;
-            tempYuShu =(tempYuShu *10 +a[j])%b;
-            k++;
+        personA.lose ++;
+        personB.win ++;
+        if(b =='J'){
+            personB.J++;
+        }else if(b == 'C'){
+            personB.C++;
+        }else{
+            personB.B++;
         }
     }
+}
 
-    for(int l =0;l<k;l++){
-        printf("%d",c[l]);
+char  complePerson(Person &person){
+    if(person.J >person.C){
+        if(person.J>person.B)
+        {
+            return 'J';
+        }else{
+            return 'B';
+        }
+    }else{
+        if(person.C>person.B){
+            return 'C';
+        }else{
+            return 'B';
+        }
     }
-    printf(" %d",tempYuShu);
+}
+// 1018 锤子剪刀布
+int main(){
+    int N = 0;
+    scanf("%d",&N);
+    char a,b;
+    int result;
+    for(int i = 0; i < N; i++){
+        scanf(" %c %c", &a, &b);
+        result= compleCJB( a, b);
+        addResult( person1, person2, result, a, b);
+    }
+    char res1 =complePerson(person1);
+    char res2 =complePerson(person2);
+    printf("%d %d %d\n", person1.win, person1.flat, person1.lose);
+    printf("%d %d %d\n", person2.win, person2.flat, person2.lose);
+    printf("%c %c", res1, res2);
     return 0;
 }
 
