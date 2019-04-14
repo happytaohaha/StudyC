16220e6c87609527fff6fcc70a87952f4fbaf663
happytaohaha <1178984504@qq.com>
2019-04-14 10:31:40 +0800

    1019 数字黑洞

--- main.cpp
+++ main.cpp
@@ -1,105 +1,73 @@
 #include<stdio.h>
 /**
- * 不知道为什么我这个编译不通过
+ * 1. 4位数
+ * 2. 0001 -1000
+ * 3. %04d 前方补0
+ * 4. 特殊情况  第一个输入  6174
  */
- //1018 锤子剪刀布
- struct Person{
-     int B = 0;
-     int C = 0;
-     int J = 0;
-     int win = 0;
-     int lose = 0;
-     int  flat= 0;
- } person1,person2;
- int compleCJB(char a, char b){
-   if(a == 'J'){
-       switch (b){
-           case 'B':
-               return 1;
-           case 'C':
-               return -1;
-           default:
-               return 0;
-       }
-   }else if(a == 'B'){
-       switch (b){
-           case 'C':
-               return 1;
-           case 'J':
-               return -1;
-           default:
-               return 0;
-       }
-   }else{
-       switch (b){
-           case 'J':
-               return 1;
-           case 'B':
-               return -1;
-           default:
-               return 0;
-       }
-   }
- };
-void addResult(Person &personA,Person &personB, int result, char a, char b){
-    if(result==1){
-        personA.win ++;
-        personB.lose ++;
-        if(a =='J'){
-            personA.J++;
-        }else if(a == 'C'){
-            personA.C++;
-        }else{
-            personA.B++;
-        }
-    }else if(result ==0){
-        personA.flat ++;
-        personB.flat ++;
-    }else{
-        personA.lose ++;
-        personB.win ++;
-        if(b =='J'){
-            personB.J++;
-        }else if(b == 'C'){
-            personB.C++;
-        }else{
-            personB.B++;
+int getMax( int a){
+    int s[4] = {};
+    int result = 0;
+    for(int i = 0; i < 4; i++){
+        s[i] = a % 10;
+        a = a / 10;
+    }
+    int temp = 0;
+    for(int i = 0; i < 4; i++){
+        for(int j = i; j < 4; j++){
+            if(s[i] < s[j]){
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
         }
     }
-}
-
-char  complePerson(Person &person){
-    if(person.J >person.C){
-        if(person.J>person.B)
-        {
-            return 'J';
-        }else{
-            return 'B';
-        }
-    }else{
-        if(person.C>person.B){
-            return 'C';
-        }else{
-            return 'B';
+    for(int i = 0; i < 4; i++){
+        result = result * 10 +s[i];
+    }
+    return result;
+};
+int getMin( int a){
+    int s[4] = {};
+    int result = 0;
+    for(int i = 0; i < 4; i++){
+        s[i] = a % 10;
+        a = a / 10;
+    }
+    int temp = 0;
+    for(int i = 0; i < 4; i++){
+        for(int j = i; j < 4; j++){
+            if(s[i] > s[j]){
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
         }
     }
-}
-// 1018 锤子剪刀布
+    for(int i = 0; i < 4; i++){
+        result = result * 10 +s[i];
+    }
+    return result;
+};
+// 1019 数字黑洞
 int main(){
-    int N = 0;
-    scanf("%d",&N);
-    char a,b;
-    int result;
-    for(int i = 0; i < N; i++){
-        scanf(" %c %c", &a, &b);
-        result= compleCJB( a, b);
-        addResult( person1, person2, result, a, b);
+    int a = 0;
+    scanf("%d", &a);
+    if(a%1111 ==0){  //特殊情况
+        printf("%4d - %4d = 0000",a,a);
+    }else{
+        int b1 = getMax(a);
+        int b2 = getMin(a);
+        a = b1 - b2;
+        printf("%d - %04d = %04d\n", b1, b2, a);
+        while(a != 6174){
+             b1 = getMax(a);
+             b2 = getMin(a);
+            a = b1 - b2;
+            printf("%d - %04d = %04d\n", b1, b2, a);
+        }
     }
-    char res1 =complePerson(person1);
-    char res2 =complePerson(person2);
-    printf("%d %d %d\n", person1.win, person1.flat, person1.lose);
-    printf("%d %d %d\n", person2.win, person2.flat, person2.lose);
-    printf("%c %c", res1, res2);
+    //排序
     return 0;
 }
 
