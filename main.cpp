ed5dbcfba7ebede0f275d8e23346fc63d4154daf
happytaohaha <1178984504@qq.com>
2019-04-14 11:47:28 +0800

    1022 D进制的A+B

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include<stdio.h>
-/**
+/** int 4字节 有符号数 +2的30次方-1      -2的30次方         unsigned int 2的31次方 - 1
+ *  1.非负整数：    0  正整数
  *
  */
- //1021 个位数统计
+ //1022 D进制的A+B
 int main(){
-    char a[1000]="";
-    int b[10]={};
-    scanf("%s", a);
-    for(int i = 0; a[i] !='\0';i++){
-        b[a[i]-'0']++;
-    }
-    for(int i = 0; i < 10 ;i++){
-        if(b[i] != 0){
-            printf("%d:%d\n", i, b[i]);
+    unsigned int A,B,D;
+    int res[31]={};
+    scanf("%d %d %d", &A, &B, &D);
+    unsigned int C = A + B;
+    int i = 0;
+    if(C == 0){
+        printf("%d", 0);
+    }else{
+        for(; C != 0;i++){
+            res[i] =C%D;
+            C = C / D;
+        }
+        i--;
+        for (; i >= 0 ; --i) {
+            printf("%d", res[i]);
         }
     }
     return 0;
